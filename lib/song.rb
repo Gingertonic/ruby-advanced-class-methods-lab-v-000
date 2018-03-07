30dd925df19d6cc56b67f4c5c1f4d007d6deaf91
Beth Schofield <bethmschofield@gmail.com>
2018-03-07 05:55:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,9 @@ class Song
   def self.find_by_name(title)
     self.all.detect {|song| song.name == title}
   end
+
+  def find_or_create_by_name(title)
+    if self.all.detect ? {|song| song.name == title} : self.create_by_name(title)
+      
+  end
 end
