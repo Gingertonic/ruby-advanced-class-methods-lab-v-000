dafcac1920de3de81e2a76a44399b571f3121360
Beth Schofield <bethmschofield@gmail.com>
2018-03-07 06:07:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
 
   def self.new_from_filename(filename)
     newsong = self.new
-    newsong.name = filename.split(/( = |\.)/)[1]
+    newsong.name = filename
     newsong.artist_name = filename.split(/( = |\.)/)[0]
     self.all << newsong
   end
