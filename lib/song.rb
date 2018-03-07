8f599fe882c15b34852b904a6c41eedac8c7bfb1
Beth Schofield <bethmschofield@gmail.com>
2018-03-07 05:58:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,6 @@ class Song
   end
 
   def self.find_or_create_by_name(title)
-    !self.find_by_name(title) ? self.create_by_name(title) : find_by_name(title) 
+    !self.find_by_name(title) ? self.create_by_name(title) : find_by_name(title)
   end
 end
