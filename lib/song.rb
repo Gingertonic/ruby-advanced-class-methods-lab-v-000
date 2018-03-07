779ee102956b4a917583d5afd9af63913bb17255
Beth Schofield <bethmschofield@gmail.com>
2018-03-07 05:43:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(title)
     song = Song.new
-    song.name= name
+    song.name= title
     self.all << song
   end
 
